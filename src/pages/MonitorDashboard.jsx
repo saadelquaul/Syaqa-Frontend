9ed3f06222c22bb4aefcfb2d4d72568c8d53308c
Saad el quaul <saadelquaul@gmail.com>
2025-04-24 11:42:51 +0100

    modifying the MonitorDashboard componenet to fetch the courses and the total courses that are been created by the monitor

--- src/pages/MonitorDashboard.jsx
+++ src/pages/MonitorDashboard.jsx
@@ -3,16 +3,47 @@ import { Calendar, BookOpen, Users, CreditCard } from "lucide-react"
 import RecentBookings from "@/components/dashboard/recent-bookings"
 import StatsCard from "@/components/dashboard/stats-card"
 import UpcomingLessons from "@/components/dashboard/upcoming-lessons"
+import axios from "axios"
+import { getAuthHeader } from "@/utils/auth"
+import { useEffect, useState } from "react"
 
 export default function MonitorDashboard() {
+
+  const [coursesCount, setCoursesCount] = useState("--");
+  const [isLoadingCourses, setIsLoadingCourses] = useState(true);
+
+  useEffect(() => {
+    const fetchCoursesData = async () => {
+      try {
+        setIsLoadingCourses(true);
+        
+        const response = await axios.get("http://localhost:8000/api/monitor/getCourses", {
+          headers: getAuthHeader()
+        });
+        
+        setCoursesCount(response.data.courses?.length || 0);
+      } catch (error) {
+        console.error("Failed to fetch courses:", error);
+        setCoursesCount("--");
+      } finally {
+        setIsLoadingCourses(false);
+      }
+    };
+    
+    fetchCoursesData();
+  }, []);
+
+
   return (
     <>
-      <h1 className="dashboard-title">Tableau de bord</h1>
+      <h1 className="dashboard-title p-5">
+        Bienvenue sur votre tableau de bord !
+      </h1>
       
       <div className="stats-grid">
         <StatsCard
           title="Cours publiés"
-          value="18"
+          value={isLoadingCourses ? "..." : coursesCount}
           icon={<BookOpen className="stats-icon" />}
         />
         <StatsCard
